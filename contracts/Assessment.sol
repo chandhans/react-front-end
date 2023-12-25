805a9e3a9eca6ac0387cc7f78e37871aa75430e2
chandhans <153435815+chandhans@users.noreply.github.com>
2023-12-25 18:14:16 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,58 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event LandBought(address buyer, uint256 landId, uint256 cost);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function concatenateStrings(string memory _str1, string memory _str2) public pure returns (string memory) {
+        // Concatenate two strings
+        return string(abi.encodePacked(_str1, _str2));
+    }
+
+    function buyLand(uint256 _landId, uint256 _cost) public {
+        require(_cost > 0, "Cost must be greater than zero");
+        require(balance >= _cost, "Insufficient funds to buy the land");
+
+        // Perform land purchase logic here
+
+        // Deduct the cost from the balance
+        balance -= _cost;
+
+        // Emit the event
+        emit LandBought(msg.sender, _landId, _cost);
     }
 }
